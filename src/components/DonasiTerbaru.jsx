e0e335941e1d18ff4f068660fe66d3241c7c1622
mayangpuspitasari <mayangpuspitas410@gmail.com>
2025-07-29 09:07:44 +0530

    url

--- src/components/DonasiTerbaru.jsx
+++ src/components/DonasiTerbaru.jsx
@@ -9,7 +9,7 @@ const DonasiTerbaru = () => {
 
   useEffect(() => {
     axios
-      .get('http://109.110.188.170:5000/program/terbaru')
+      .get('https://109.110.188.170:5000/program/terbaru')
       .then((res) => {
         setDonasi(res.data);
         setIsLoading(false);
